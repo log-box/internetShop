02729321c32a129cce7a4a45087a08873fedffdb
log-box <a.y.logvin@gmail.com>
2021-06-29 14:05:17 +0300

    done lesson 3 (do random output products)

--- logbox_shop/mainapp/views.py
+++ logbox_shop/mainapp/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-
+import random
 from logbox_shop.views import getjson
-from mainapp.models import ProductCategory
+from mainapp.models import ProductCategory, Product
 
 
 def products(request):
     title = 'магазин/продукция'
     products_category = ProductCategory.objects.all()
+    len = Product.objects.all().__len__()
+    rand_start = random.randint(0, len)
+    rand_end = random.randint(0, len)
+    if rand_start > rand_end:
+        rand_start, rand_end = rand_end, rand_start
+    products = Product.objects.all()[rand_start:rand_end]
     context = {
         'general_menu_links': getjson('general_menu_links'),
         'title': title,
         # 'links_menu': getjson('links_menu'),
         'products_category': products_category,
+        'products': products,
     }
     return render(request, 'products.html', context)
 
