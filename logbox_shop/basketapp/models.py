ed2ff7ac92781554ee0bfb0a4eb596b7ada20c23
log-box <a.y.logvin@gmail.com>
2021-07-05 16:56:33 +0300

    lesson5 in process...

---
+++ logbox_shop/basketapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import Sum
+
+from logbox_shop import settings
+from mainapp.models import Product
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='basket',
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.PositiveIntegerField(
+        verbose_name='количество',
+        default=0,
+    )
+    add_datetime = models.DateTimeField(
+        verbose_name='время',
+        auto_now_add=True,
+    )
+
+    def total_sum(self, user):
+        # field_name_sum = Basket.objects.aggregate(Sum('field_name'))
+        pass
--- logbox_shop/mainapp/views.py
+++ logbox_shop/mainapp/views.py
@@ -1,14 +1,18 @@
 import random
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from logbox_shop.views import getjson
 from mainapp.models import ProductCategory, Product
 
 
-def products(request, pk=None):
+def products(request):
     title = 'магазин/продукция'
     products_category_menu = ProductCategory.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     # вычислям длину (кол-во) продуктов всего для рандомного числа в этом диапазоне
     _len = Product.objects.all().__len__()
     rand_start = random.randint(0, _len)
@@ -23,6 +27,7 @@ def products(request, pk=None):
         'title': title,
         'products_category_menu': products_category_menu,
         'products': products,
+        'basket': basket,
     }
     #######################################################################################################
     return render(request, 'products.html', context)
@@ -30,8 +35,22 @@ def products(request, pk=None):
 
 def group_of_products(request, slug):
     title = f'магазин/продукция/{slug}'
+    products_category_menu = ProductCategory.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+    if slug == 'products_all':
+        products = Product.objects.all().order_by('price')
+        category = {'name': 'все'}
+    else:
+        products = Product.objects.filter(category__href=slug).order_by('price')
+        category = get_object_or_404(ProductCategory, href=slug)
     context = {
         'general_menu_links': getjson('general_menu_links'),
         'title': title,
+        'products_category_menu': products_category_menu,
+        'products': products,
+        'category': category,
+        'basket': basket,
     }
-    return render(request, f'{slug}.html', context)
+    return render(request, 'groups_products.html', context)
