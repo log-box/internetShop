d1dfd1db701439ac3601f76474264c0e7fea7460
artem <a.y.logvin@gmail.com>
2021-07-05 06:07:44 +0300

    added lesson5

--- logbox_shop/mainapp/views.py
+++ logbox_shop/mainapp/views.py
@@ -6,13 +6,13 @@ from logbox_shop.views import getjson
 from mainapp.models import ProductCategory, Product
 
 
-def products(request):
+def products(request, pk=None):
     title = 'магазин/продукция'
-    products_category = ProductCategory.objects.all()
+    products_category_menu = ProductCategory.objects.all()
     # вычислям длину (кол-во) продуктов всего для рандомного числа в этом диапазоне
-    len = Product.objects.all().__len__()
-    rand_start = random.randint(0, len)
-    rand_end = random.randint(0, len)
+    _len = Product.objects.all().__len__()
+    rand_start = random.randint(0, _len)
+    rand_end = random.randint(0, _len)
     if rand_start > rand_end:
         rand_start, rand_end = rand_end, rand_start
     ###############################################################################
@@ -21,7 +21,7 @@ def products(request):
     context = {
         'general_menu_links': getjson('general_menu_links'),
         'title': title,
-        'products_category': products_category,
+        'products_category_menu': products_category_menu,
         'products': products,
     }
     #######################################################################################################
