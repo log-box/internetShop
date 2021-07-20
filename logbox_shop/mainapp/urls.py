e46606cc38198982afb266c785b62061c053364e
log-box <a.y.logvin@gmail.com>
2021-07-20 15:08:29 +0300

    lesson8 in progress ... mainapp writen on class engine

--- logbox_shop/mainapp/urls.py
+++ logbox_shop/mainapp/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 import mainapp.models
-from .views import products, group_of_products, product, ProductsView
+from .views import *
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', ProductsView.as_view(), name='products'),
-    path('<slug:slug>/', group_of_products, name='group_of_products'),
-    path('<slug:slug>/page/<int:page>/', group_of_products, name='page'),
-    path('product/<int:pk>/', product, name='product'),
+    path('<slug:slug>/', GroupProductsView.as_view(), name='group_of_products'),
+    path('<slug:slug>/page/<int:page>/', GroupProductsView.as_view(), name='page'),
+    path('product/<int:pk>/', ProductView.as_view(), name='product'),
 ]
--- logbox_shop/mainapp/views.py
+++ logbox_shop/mainapp/views.py
@@ -1,10 +1,8 @@
 import random
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render, get_object_or_404
-from django.views import View
-from django.views.generic import DetailView
-from django.views.generic.base import ContextMixin, TemplateResponseMixin, TemplateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 
 from basketapp.models import Basket
 from logbox_shop.views import getjson
@@ -30,70 +28,58 @@ class ProductsView(TemplateView):
     template_name = 'products.html'
 
     def get_context_data(self, **kwargs):
-        extra_context = super().get_context_data(**kwargs)
-        extra_context['general_menu_links'] = getjson('general_menu_links')
-        extra_context['title'] = 'Магазин/Продукция'
-        extra_context['general_menu_links'] = ProductCategory.objects.all()
-        extra_context['same_products'] = get_same_products(get_hot_product())
-        extra_context['hot_product'] = get_hot_product()
-        extra_context['basket'] = get_basket(self.request.user)
-        return extra_context
+        context = super().get_context_data(**kwargs)
+        context['general_menu_links'] = getjson('general_menu_links')
+        context['title'] = 'Магазин/Продукция'
+        context['products_category_menu'] = ProductCategory.objects.all()
+        context['same_products'] = get_same_products(get_hot_product())
+        context['hot_product'] = get_hot_product()
+        context['basket'] = get_basket(self.request.user)
+        return context
 
 
-def products(request):
-    title = 'магазин/продукция'
-    hot_product = get_hot_product()
-    context = {
-        'general_menu_links': getjson('general_menu_links'),
-        'title': title,
-        'products_category_menu': ProductCategory.objects.all(),
-        'same_products': get_same_products(hot_product),
-        'hot_product': hot_product,
-        'basket': get_basket(request.user),
-    }
-    return render(request, 'products.html', context)
+class ProductView(TemplateView):
+    template_name = 'product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Продукты/{(get_object_or_404(Product, pk=self.kwargs.get("pk"))).short_desc}'
+        context['general_menu_links'] = getjson('general_menu_links')
+        context['products_category_menu'] = ProductCategory.objects.all()
+        context['product'] = get_object_or_404(Product, pk=self.kwargs.get("pk")),
+        context['basket'] = get_basket(self.request.user)
+        return context
 
 
-def product(request, pk):
-    title = f'Продукты/{(get_object_or_404(Product, pk=pk)).short_desc}'
-    context = {
-        'title': title,
-        'general_menu_links': getjson('general_menu_links'),
-        'products_category_menu': ProductCategory.objects.all(),
-        'product': get_object_or_404(Product, pk=pk),
-        'basket': get_basket(request.user),
-    }
+class GroupProductsView(ListView):
+    template_name = 'groups_products.html'
+    context_object_name = 'products'
+    paginate_by = 2
 
-    return render(request, 'product.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['general_menu_links'] = getjson('general_menu_links')
+        context['products_category_menu'] = ProductCategory.objects.all()
+        basket = []
+        if self.request.user.is_authenticated:
+            basket = get_basket(self.request.user)
+        context['basket'] = basket
+        if self.kwargs.get('slug') == 'products_all':
+            category = {
+                'name': 'все',
+                'href': 'products_all',
+            }
+            context['title'] = f'Продукты/{category["name"]}'
+        else:
+            category = get_object_or_404(ProductCategory, href=self.kwargs.get('slug'))
+            context['title'] = f'Продукты/{category.name}'
+        context['category'] = category
 
+        return context
 
-def group_of_products(request, slug, page=2):
-    title = f'магазин/продукция/{slug}'
-    basket = []
-    if request.user.is_authenticated:
-        basket = get_basket(request.user)
-    if slug == 'products_all':
-        products = Product.objects.filter(is_deleted=False).order_by('price')
-        category = {
-            'name': 'все',
-            'href': 'products_all',
-        }
-    else:
-        products = Product.objects.filter(category__href=slug, is_deleted=False)
-        category = get_object_or_404(ProductCategory, href=slug)
-    paginator = Paginator(products, 2)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-    context = {
-        'general_menu_links': getjson('general_menu_links'),
-        'title': title,
-        'products_category_menu': ProductCategory.objects.all(),
-        'products': products_paginator,
-        'category': category,
-        'basket': basket,
-    }
-    return render(request, 'groups_products.html', context)
+    def get_queryset(self):
+        if self.kwargs.get('slug') == 'products_all':
+            products = Product.objects.filter(is_deleted=False).order_by('price')
+        else:
+            products = Product.objects.filter(category__href=self.kwargs.get('slug'), is_deleted=False)
+        return products
